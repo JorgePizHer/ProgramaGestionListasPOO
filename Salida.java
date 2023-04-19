a4edb3d4c52d64aa6356dc596dc8e89541a7adb5
JorgePizHer <119115105+JorgePizHer@users.noreply.github.com>
2023-04-19 20:12:24 +0200

    Clase comparador. Métodos buscar y eliminar

--- Salida.java
+++ Salida.java
@@ -1,15 +1,7 @@
 import java.util.Scanner;
 public class Salida {
 
-		/**
-		 * Muestra un mensaje de bienvenida al usuario cuando inicia el programa.
-		 * Pregunta al usuario que opcion del menu quiere ejecutar.
-		 * 
-		 * @author Jorge Pizarro
-		 * @param texto    El mensaje de bienvenida al usuario.
-		 * @param opciones Pregunta para introducir las opciones del menu al usuario.
-		 * @version 1.3
-		 */
+		
 
 		public static void presentacion(String texto) {
 
@@ -18,14 +10,7 @@ public class Salida {
 			
 		}
 
-		/**
-		 * Muestra el menu al usuario. Permite al usuario seleccionar la opción del menu
-		 * que desee.
-		 * 
-		 * @author Jorge Pizarro
-		 * @return La opcion del menu seleccionada por el usuario.
-		 * @version 1.3
-		 */
+		
 
 		public static int mostrarMenu() {
 			int seleccion;
@@ -44,22 +29,14 @@ public class Salida {
 			return seleccion;
 		}
 		
-		/**
-		 * Cuando el usuario selecciona la opción del menu de "Buscar", se muestra un
-		 * nuevo menu para buscar por diferentes datos. Permite al usuario seleccionar
-		 * el tipo de busqueda que desea.
-		 * 
-		 * @author Jorge Pizarro
-		 * @return La opcion del menu de busqueda seleccionada por el usuario.
-		 * @version 1.3
-		 */
+		
 		public static int menuModificar() {
 			
 			int seleccion;
 			System.out.println("Seleccione lo que desea modificar");
 			System.out.println("");
 
-			System.out.println("1-Modificar todo el contenido");
+			System.out.println("1-Modificar todo el contenido de un anime");
 			System.out.println("2-Modificar nombre");
 			System.out.println("3-Modificar demografía");
 			System.out.println("4-Modificar género");
@@ -76,7 +53,26 @@ public class Salida {
 		}
 	
 	
-	
+			public static int menuBuscar() {
+			
+			int seleccion;
+			
+			System.out.println("Seleccione el elemento por el que desea buscar");
+			System.out.println("");
+
+			System.out.println("1-Buscar por nombre");
+			System.out.println("2-Buscar por demografía");
+			System.out.println("3-Buscar por género");
+			System.out.println("4-Buscar por número de episodios");
+			System.out.println("5-Buscar por valoración");
+			System.out.println("");
+
+			Scanner lector = new Scanner(System.in);
+			seleccion = lector.nextInt();
+			lector.nextLine();
+
+			return seleccion;
+		}
 	
 	
 	
