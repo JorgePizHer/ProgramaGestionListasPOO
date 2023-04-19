e243c9043cf0a00e11b7ef9992568cb84f3dbc91
JorgePizHer <119115105+JorgePizHer@users.noreply.github.com>
2023-04-19 16:58:20 +0200

    Métodos añadir y modificar implementados y funcionales. Clase Salida para interacción con el usuario por consola creada

--- ListaAnimes.java
+++ ListaAnimes.java
@@ -47,6 +47,30 @@ public class ListaAnimes {
 	}
 
 
+	
+	public void gestionarAccion(int seleccion, String nombre, String demografia, String genero, int episodios, int valoracion) {
+		
+		
+		switch (seleccion) {
+		case 1:
+			addAnime(nombre, demografia, genero, episodios, valoracion);
+			break;
+		case 2:
+			modificarAnime(nombre);
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		case 5:
+			System.exit(0);
+		}	
+	}
+	
+	
+	
 
 	public void addAnime(String nombre, String demografia, String genero, int episodios, int valoracion) {
 
@@ -80,8 +104,176 @@ public class ListaAnimes {
 	}
 	
 	
+	public void modificarAnime(String nombreAnime) {
+
+		int seleccion;
+		String nuevoNombre="";
+		String nuevaDemografia="";
+		String nuevoGenero="";
+		int nuevosEpisodios=0;
+		int nuevaValoracion=0;
+		
+		Scanner lector = new Scanner(System.in);
+		
+		do {
+		
+		seleccion=Salida.menuModificar();
+			
+		switch (seleccion) {
+			case 1: 
+				
+				
+				System.out.println("Nombre del anime que desea modificar: ");
+				nombreAnime = lector.nextLine();
+				
+				
+				for(Anime item: listaAnimes) {
+					
+					if(item.getNombre().equals(nombreAnime)) {
+			
+						System.out.println(item);
+						
+				System.out.println("Introduzca el nuevo nombre: ");
+				nuevoNombre = lector.nextLine();	
+				item.setNombre(nuevoNombre);
+				
+				System.out.println("Introduzca la nueva demografía: ");
+				nuevaDemografia = lector.nextLine();	
+				item.setDemografia(nuevaDemografia);
+						
+				System.out.println("Introduzca el nuevo género: ");
+				nuevoGenero = lector.nextLine();	
+				item.setGenero(nuevoGenero);
+				
+				System.out.println("Introduzca el nuevo número de episodios: ");
+				nuevosEpisodios = lector.nextInt();
+				lector.nextLine();
+				item.setEpisodios(nuevosEpisodios);
+				
+				System.out.println("Introduzca la nueva valoración: ");
+				nuevaValoracion = lector.nextInt();
+				lector.nextLine();
+				item.setValoracion(nuevaValoracion);
+				
+				System.out.println(listaAnimes.toString());
+				System.out.println("");
+					
+					}
+				}
+				break;
+			case 2:
+				
+				System.out.println("Nombre del anime que desea modificar: ");
+				nombreAnime = lector.nextLine();
+				
+				
+				for(Anime item: listaAnimes) {
+					
+					if(item.getNombre().equals(nombreAnime)) {
+			
+						System.out.println(item);
+						
+				System.out.println("Introduzca el nuevo nombre: ");
+				nuevoNombre = lector.nextLine();	
+				item.setNombre(nuevoNombre);
+					}
+				}
+				
+				
+				
+				break;
+			case 3:
+				
+				System.out.println("Nombre del anime que desea modificar: ");
+				nombreAnime = lector.nextLine();
+				
+				
+				for(Anime item: listaAnimes) {
+					
+					if(item.getNombre().equals(nombreAnime)) {
+			
+						System.out.println(item);
+						
+				System.out.println("Introduzca la nueva demografía: ");
+				nuevaDemografia = lector.nextLine();	
+				item.setDemografia(nuevaDemografia);
+					}
+				}
+				
+				break;
+			case 4:
+				
+				System.out.println("Nombre del anime que desea modificar: ");
+				nombreAnime = lector.nextLine();
+				
+				
+				for(Anime item: listaAnimes) {
+					
+					if(item.getNombre().equals(nombreAnime)) {
+			
+						System.out.println(item);
+						
+				System.out.println("Introduzca el nuevo género: ");
+				nuevoGenero = lector.nextLine();	
+				item.setGenero(nuevoGenero);
+					}
+				}
+				
+				break;
+				
+			case 5:
+				
+				System.out.println("Nombre del anime que desea modificar: ");
+				nombreAnime = lector.nextLine();
+				
+				
+				for(Anime item: listaAnimes) {
+					
+					if(item.getNombre().equals(nombreAnime)) {
+			
+						System.out.println(item);
+						
+				System.out.println("Introduzca el nuevo número de episodios: ");
+				nuevosEpisodios = lector.nextInt();
+				lector.nextLine();
+				item.setEpisodios(nuevosEpisodios);
+					}
+				}
+				
+				break;
+				
+			case 6:
+				
+				System.out.println("Nombre del anime que desea modificar: ");
+				nombreAnime = lector.nextLine();
+				
+				
+				for(Anime item: listaAnimes) {
+					
+					if(item.getNombre().equals(nombreAnime)) {
+			
+						System.out.println(item);
+						
+				System.out.println("Introduzca la nueva valoración: ");
+				nuevaValoracion = lector.nextInt();
+				lector.nextLine();
+				item.setValoracion(nuevaValoracion);
+				
+					}
+				}
+				
+				
+				break;
+				
+		}
+			
+		
+		}while (seleccion < 1|| seleccion >6);
+		
+		
+		
+		
 	
 	
-	
-	
+	}	
 }
